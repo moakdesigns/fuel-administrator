76ae7bdbf23ca5747e46862f432f28711b6ac105
Marcus <webstone@gmail.com>
2012-12-15 15:24:27 +0100

    theme functionality
    Added theme functionality and "replaced" the core function "Theme" with
    my own one,
    to use a modules folder in the themes to get a better structure

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -1,20 +1,102 @@
 <?php
-/**
- * Fuel is a fast, lightweight, community driven PHP5 framework.
- *
- * @package    Fuel
- * @version    1.0
- * @author     Fuel Development Team
- * @license    MIT License
- * @copyright  2010 - 2012 Fuel Development Team
- * @link       http://fuelphp.com
- */
-
-/**
- * If you want to override the default configuration, add the keys you
- * want to change here, and assign new values to them.
- */
 
 return array(
+	'base_url' => NULL,
+	'url_suffix' => '',
+	'index_file' => false,
+	'profiling' => false,
+	'cache_dir' => APPPATH.'cache/',
+	'caching' => false,
+	'cache_lifetime' => 3600,
+	'ob_callback' => NULL,
+	'errors' => 
+	array(
+		'continue_on' => 
+		array(
+		),
+		'throttle' => 10,
+		'notices' => true,
+	),
+	'language' => 'en',
+	'language_fallback' => 'en',
+	'locale' => 'en_US',
+	'encoding' => 'UTF-8',
+	'server_gmt_offset' => 0,
+	'default_timezone' => NULL,
+	'log_threshold' => 2,
+	'log_path' => APPPATH.'logs/',
+	'log_date_format' => 'Y-m-d H:i:s',
+	'security' => 
+	array(
+		'csrf_autoload' => false,
+		'csrf_token_key' => 'fuel_csrf_token',
+		'csrf_expiration' => 0,
+		'uri_filter' => 
+		array(
+			0 => 'htmlentities',
+		),
+		'input_filter' => 
+		array(
+		),
+		'output_filter' => 
+		array(
+			0 => 'Security::htmlentities',
+		),
+		'htmlentities_flags' => 3,
+		'htmlentities_double_encode' => false,
+		'auto_filter_output' => true,
+		'whitelisted_classes' => 
+		array(
+			0 => 'Fuel\\Core\\Response',
+			1 => 'Fuel\\Core\\View',
+			2 => 'Fuel\\Core\\ViewModel',
+			3 => 'Closure',
+		),
+	),
+	'cookie' => 
+	array(
+		'expiration' => 0,
+		'path' => '/',
+		'domain' => NULL,
+		'secure' => false,
+		'http_only' => false,
+	),
+	'validation' => 
+	array(
+		'global_input_fallback' => true,
+	),
+	'controller_prefix' => 'Controller_',
+	'routing' => 
+	array(
+		'case_sensitive' => true,
+		'strip_extension' => true,
+	),
+	'module_paths' => array(
+		APPPATH.'modules'.DS
+	),
 
+	'package_paths' => 
+	array(
+		PKGPATH
+	),
+	'always_load' => 
+	array(
+		'packages' => 
+		array(
+		),
+		'modules' => 
+		array(
+		),
+		'classes' => 
+		array(
+		),
+		'config' => 
+		array(
+		),
+		'language' => 
+		array(
+		),
+	),
 );
+
+/* End of file config.php */
