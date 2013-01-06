69a70faf373e01775c0b4b458ec0af44ac99c3d5
Marcus Reinhardt <webstone@gmail.com>
2013-01-06 14:32:51 +0100

    added sql file

--- fuel/app/modules/users/classes/controller/users.php
+++ fuel/app/modules/users/classes/controller/users.php
@@ -13,49 +13,58 @@ class Controller_Users extends \Controller_Base
 
     }
     
-        public function action_index($userid=NULL)
-        {
-            \Response::redirect('login');
-        }
+    /**
+     * Index function - only a redirect to the login page
+     * @access public
+     * @param  [number] $userid 
+     * @return void
+     */
+    public function action_index($userid=NULL)
+    {
+        \Response::redirect('login');
+    }
     
+    /**
+     * Login function
+     * @return void
+     */
 	public function action_login()
 	{
-            //check that the current user is not already logged in
-            if ( \Warden::check())
+        //check that the current user is not already logged in
+        if ( \Warden::check())
+        {
+            \Messages::warning( \Lang::get('login_already') );
+            \Response::redirect('/');
+        }
+        
+        
+        if (\Input::method() === 'POST') 
+        {
+            try
             {
-                \Messages::warning( \Lang::get('login_already') );
-                \Response::redirect('/');
-            }
-            
-            
-            if (\Input::method() === 'POST') 
-            {
-                try
+                $remember   = (\Input::post('remember_me')==1) ? true : false;
+                $user       = \Warden::authenticate(\Input::post('username_or_email'), \Input::post('password'), $remember);
+                if($user)
                 {
-                    $remember   = (\Input::post('remember_me')==1) ? true : false;
-                    $user       = \Warden::authenticate(\Input::post('username_or_email'), \Input::post('password'), $remember);
-                    if($user)
-                    {
-                        //login was successful
-                        \Messages::success( \Lang::get('login_success') );
-                        \Response::redirect('/');
-                    }
-                    else
-                    {
-                        //invalid username and/or password
-                        \Messages::error( \Lang::get('login_error') );
-                        \Response::redirect('login');
-
-                    }
-                } 
-                catch (\Warden\Warden_Failure $ex)
-                {
-                    //something goes wrong in the warden package
-                    \Messages::error($ex->getMessage());
-                    \Response::redirect('login');
+                    //login was successful
+                    \Messages::success( \Lang::get('login_success') );
+                    \Response::redirect('/');
                 }
+                else
+                {
+                    //invalid username and/or password
+                    \Messages::error( \Lang::get('login_error') );
+                    \Response::redirect('login');
+
+                }
+            } 
+            catch (\Warden\Warden_Failure $ex)
+            {
+                //something goes wrong in the warden package
+                \Messages::error($ex->getMessage());
+                \Response::redirect('login');
             }
-            
+        }
 
         return \Theme::instance()
                         ->get_template()
@@ -76,73 +85,215 @@ class Controller_Users extends \Controller_Base
         \Messages::success( \Lang::get('logout_success') );
 		\Response::redirect('/');
 	}
-        
-        public function action_register()
+    
+    /**
+     * the register action
+     * @return void
+     */
+    public function action_register()
+    {
+        //check that the current user is not already logged in
+        if ( \Warden::check())
         {
-            //check that the current user is not already logged in
-            if ( \Warden::check())
+            \Messages::warning( \Lang::get('login_already') );
+            \Response::redirect('/');
+        }
+        
+        $val = \Validation::forge();
+        $val->add_callable('myvalidation');
+        $val->add_field('username', 'Username', 'required|min_length[3]|max_length[20]|unique[users.username]');
+        $val->add_field('password', 'Password', 'required|min_length[6]|max_length[20]');
+        $val->add_field('email', 'E-Mail', 'required|valid_email|unique[users.email]');
+        if ( $val->run() )
+        {
+            $user = new \Warden\Model_User(array(
+                                			'username' => $val->validated('username'),
+                                			'password' => $val->validated('password'),
+                                			'email'	   => $val->validated('email'),
+                                		  )
+            );
+	
+            if( $user->save() )
             {
-                \Messages::warning( \Lang::get('login_already') );
+                
+                \Messages::success( \Lang::get('register_success') );
                 \Response::redirect('/');
             }
-            
-            $val = \Validation::forge();
-            $val->add_callable('myvalidation');
-            $val->add_field('username', 'Username', 'required|min_length[3]|max_length[20]|unique[users.username]');
-            $val->add_field('password', 'Password', 'required|min_length[6]|max_length[20]');
-            $val->add_field('email', 'E-Mail', 'required|valid_email|unique[users.email]');
-            if ( $val->run() )
+            else
             {
-                $user = new \Warden\Model_User(array(
-                                    			'username' => $val->validated('username'),
-                                    			'password' => $val->validated('password'),
-                                    			'email'	   => $val->validated('email'),
-                                    		  )
-                );
-		
-                if( $user->save() )
+                \Messages::error( \Lang::get('register_error') );
+            }
+        }
+        else
+        {
+                \Messages::error( $val->error());
+        }
+
+        return \Theme::instance()
+                    ->get_template()
+                    ->set(  'content', 
+                            \Theme::instance()->view('register')
+                        );
+    }
+
+    /**
+     * THe password forgot action
+     * 
+     * @access public
+     * @return void
+     */
+    public function action_forgot() 
+    {
+        if ( \Warden::check())
+        {
+            \Response::redirect('/');
+        }
+        $val = \Validation::forge();
+        $val->add_field('email', 'E-Mail', 'required|valid_email');
+        
+        if ( $val->run() )
+        {
+            $user = \Warden\Model_User::find('first', array('where' => array('email' => \Input::post('email'))));
+            if ($user) 
+            {
+                try 
                 {
-                    
-                    \Messages::success( \Lang::get('register_success') );
+                    $user->send_reset_password_instructions();
+                    \Messages::info( \Lang::get('forgot_success') );
                     \Response::redirect('/');
-                }
-                else
+                } 
+                catch (Exception $ex) 
                 {
-                    \Messages::error( \Lang::get('register_error') );
+                    \Messages::error($ex->getMessage());
+                    // echo sprintf('Oops, something went wrong: %s', $ex->getMessage());
                 }
             }
             else
             {
-                    \Messages::error( $val->error());
+                \Messages::error( \Lang::get('forgot_error') );
             }
-
-            return \Theme::instance()
-                        ->get_template()
-                        ->set(  'content', 
-                                \Theme::instance()->view('register')
-                            );
         }
+        else
+        {
+                \Messages::error($val->error());
+        }
+        
 
-    
-        
-        
-        public function action_forgot() {
-            if ( \Warden::check())
+        return \Theme::instance()
+                    ->get_template()
+                    ->set(  'content', 
+                            \Theme::instance()->view('forgot')
+                        );
+
+    }
+
+    /**
+     * The Password reset action
+     * 
+     * @access public
+     * @return void
+     */
+    public function action_reset()
+    {
+        // Resetting the password
+        if ( \Warden::check())
+        {
+            \Response::redirect('/');
+        }
+        $val = \Validation::forge();
+        $val->add_field('new_password', 'Password', 'required|min_length[6]|max_length[20]');
+        if ( $val->run() )
+        {
+            try 
             {
+                $user = \Warden\Model_User::reset_password_by_token(\Uri::segment(3), \Input::post('new_password'));
+
+                if ($user) {
+                    \Messages::success( \Lang::get('reset_success') );
+                    \Response::redirect('login');
+
+                } else {
+                    \Messages::error( \Lang::get('reset_error') );
+                    \Response::redirect('/');
+
+                }
+            } 
+            catch (Exception $ex) 
+            {
+                // something went wrong
+            // echo sprintf('Oops, something went wrong: %s', $ex->getMessage());
+                \Messages::error($ex->getMessage());
                 \Response::redirect('/');
+
+
             }
-            $val = \Validation::forge();
-            $val->add_field('email', 'E-Mail', 'required|valid_email');
-            
-            if ( $val->run() )
+        }
+        else
+        {
+            \Messages::error($val->error());
+        }
+        
+        return \Theme::instance()
+                    ->get_template()
+                    ->set(  'content', 
+                            \Theme::instance()->view('reset')
+                        );
+    }
+
+
+    /**
+     * The account confirmation action
+     * 
+     * @access public
+     * @param String    token   the confirm token
+     * @return void
+     */
+    public function action_confirm($token=null) 
+    {
+        
+        if ( \Warden::check())
+        {
+            \Response::redirect('/');
+        }
+        
+        if($token!=null)
+        {
+            try {
+                \Warden\Model_User::confirm_by_token($token);
+
+
+                    \Messages::success( \Lang::get('confirm_success') );
+                    \Response::redirect('login');
+
+
+            } catch (\Warden\Warden_Failure $ex) {
+                // something went wrong
+                \Messages::error($ex->getMessage());
+            \Response::redirect('confirm');
+
+            }
+        }
+        
+        $val = \Validation::forge();
+        $val->add_field('email', 'E-Mail', 'required|valid_email');
+        
+        if ( $val->run() )
+        {
+            $user = \Warden\Model_User::find('first', array('where' => array('email' => \Input::post('email'))));
+            if ($user) 
             {
-                $user = \Warden\Model_User::find('first', array('where' => array('email' => \Input::post('email'))));
-                if ($user) 
+                if($user->is_confirmed == 1)
+                {    
+                    \Messages::error( \Lang::get('confirm_error') );
+                }
+                else
                 {
                     try 
                     {
-                        $user->send_reset_password_instructions();
-                        \Messages::info( \Lang::get('forgot_success') );
+                        $user->confirmation_token = null;
+                        $user->save();
+                        $user->send_confirmation_instructions();
+                        \Messages::info( \Lang::get('confirm_success') );
                         \Response::redirect('/');
                     } 
                     catch (Exception $ex) 
@@ -151,144 +302,46 @@ class Controller_Users extends \Controller_Base
                         // echo sprintf('Oops, something went wrong: %s', $ex->getMessage());
                     }
                 }
-                else
-                {
-                    \Messages::error( \Lang::get('forgot_error') );
-                }
             }
             else
             {
-                    \Messages::error($val->error());
+                \Messages::error( \Lang::get('confirm_error') );
             }
-            
-
-            return \Theme::instance()
-                        ->get_template()
-                        ->set(  'content', 
-                                \Theme::instance()->view('forgot')
-                            );
-
         }
-
-        public function action_reset()
+        else
         {
-            // Resetting the password
-            if ( \Warden::check())
-            {
-                \Response::redirect('/');
-            }
-            $val = \Validation::forge();
-            $val->add_field('new_password', 'Password', 'required|min_length[6]|max_length[20]');
-            if ( $val->run() )
-            {
-                try 
-                {
-                    $user = \Warden\Model_User::reset_password_by_token(\Uri::segment(3), \Input::post('new_password'));
-
-                    if ($user) {
-                        \Messages::success( \Lang::get('reset_success') );
-                        \Response::redirect('login');
-
-                    } else {
-                        \Messages::error( \Lang::get('reset_error') );
-                        \Response::redirect('/');
-
-                    }
-                } 
-                catch (Exception $ex) 
-                {
-                    // something went wrong
-                // echo sprintf('Oops, something went wrong: %s', $ex->getMessage());
-                    \Messages::error($ex->getMessage());
-                    \Response::redirect('/');
-
-
-                }
-            }
-            else
-            {
                 \Messages::error($val->error());
-            }
-            
-            return \Theme::instance()
-                        ->get_template()
-                        ->set(  'content', 
-                                \Theme::instance()->view('reset')
-                            );
         }
         
-        public function action_confirm($token=null) 
+        
+       return \Theme::instance()
+                    ->get_template()
+                    ->set(  'content', 
+                            \Theme::instance()->view('confirm')
+                        );
+    }
+
+
+    /**
+     * 
+     * The user profile action
+     * 
+     * @access public
+     * @return void
+     */
+    public function action_profile()
+    {
+        //check that the current user is already logged in
+        if ( !\Warden::check() )
         {
-            
-            if ( \Warden::check())
-            {
-                \Response::redirect('/');
-            }
-            
-            if($token!=null)
-            {
-                try {
-                    \Warden\Model_User::confirm_by_token($token);
-
-
-                        \Messages::success( \Lang::get('confirm_success') );
-                        \Response::redirect('login');
-
-
-                } catch (\Warden\Warden_Failure $ex) {
-                    // something went wrong
-                    \Messages::error($ex->getMessage());
-                \Response::redirect('confirm');
-
-                }
-            }
-            
-            $val = \Validation::forge();
-            $val->add_field('email', 'E-Mail', 'required|valid_email');
-            
-            if ( $val->run() )
-            {
-                $user = \Warden\Model_User::find('first', array('where' => array('email' => \Input::post('email'))));
-                if ($user) 
-                {
-                    if($user->is_confirmed == 1)
-                    {    
-                        \Messages::error( \Lang::get('confirm_error') );
-                    }
-                    else
-                    {
-                        try 
-                        {
-                            $user->confirmation_token = null;
-                            $user->save();
-                            $user->send_confirmation_instructions();
-                            \Messages::info( \Lang::get('confirm_success') );
-                            \Response::redirect('/');
-                        } 
-                        catch (Exception $ex) 
-                        {
-                            \Messages::error($ex->getMessage());
-                            // echo sprintf('Oops, something went wrong: %s', $ex->getMessage());
-                        }
-                    }
-                }
-                else
-                {
-                    \Messages::error( \Lang::get('confirm_error') );
-                }
-            }
-            else
-            {
-                    \Messages::error($val->error());
-            }
-            
-            
-           return \Theme::instance()
-                        ->get_template()
-                        ->set(  'content', 
-                                \Theme::instance()->view('confirm')
-                            );
+            \Messages::warning( 'Sorry, but you have to logged in to change your profile.' );
+            \Response::redirect('/');
         }
+
+        
+
+
+    }
             
         
         
